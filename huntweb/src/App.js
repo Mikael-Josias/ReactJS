7f58e61ae998483c364699590879e19dc303cd0f
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-05 18:44:18 -0300

    9.Configuring Navigation

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import api from './services/api';
 
 import './style.css';
 import Header from './components/Header';
-import Main from './pages/main';
+
+import Routes from './routes';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
